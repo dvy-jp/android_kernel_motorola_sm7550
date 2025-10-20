ac707bbfa363f3cd98a14e7bea851182d8e906fe
Sergey Bashirov <sergeybashirov@gmail.com>
2025-10-20 08:56:00 -0400

    NFSD: Rework encoding and decoding of nfsd4_deviceid
    [ Upstream commit 832738e4b325b742940761e10487403f9aad13e8 ]
    
    Compilers may optimize the layout of C structures, so we should not rely
    on sizeof struct and memcpy to encode and decode XDR structures. The byte
    order of the fields should also be taken into account.
    
    This patch adds the correct functions to handle the deviceid4 structure
    and removes the pad field, which is currently not used by NFSD, from the
    runtime state. The server's byte order is preserved because the deviceid4
    blob on the wire is only used as a cookie by the client.
    
    Signed-off-by: Sergey Bashirov <sergeybashirov@gmail.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Stable-dep-of: d68886bae76a ("NFSD: Fix last write offset handling in layoutcommit")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -566,18 +566,6 @@ nfsd4_decode_state_owner4(struct nfsd4_compoundargs *argp,
 }
 
 #ifdef CONFIG_NFSD_PNFS
-static __be32
-nfsd4_decode_deviceid4(struct nfsd4_compoundargs *argp,
-		       struct nfsd4_deviceid *devid)
-{
-	__be32 *p;
-
-	p = xdr_inline_decode(argp->xdr, NFS4_DEVICEID4_SIZE);
-	if (!p)
-		return nfserr_bad_xdr;
-	memcpy(devid, p, sizeof(*devid));
-	return nfs_ok;
-}
 
 static __be32
 nfsd4_decode_layoutupdate4(struct nfsd4_compoundargs *argp,
@@ -1680,7 +1668,8 @@ nfsd4_decode_getdeviceinfo(struct nfsd4_compoundargs *argp,
 {
 	__be32 status;
 
-	status = nfsd4_decode_deviceid4(argp, &gdev->gd_devid);
+	memset(gdev, 0, sizeof(*gdev));
+	status = nfsd4_decode_deviceid4(argp->xdr, &gdev->gd_devid);
 	if (status)
 		return status;
 	if (xdr_stream_decode_u32(argp->xdr, &gdev->gd_layout_type) < 0)

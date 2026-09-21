e0129375ddf2882bd7c24b89b3d25466c7f46142
deivy <deivy@local>
2026-09-21 14:41:05 -0300

    kernel 221: preserve BPF attach-type compatibility on Motorola

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1848,10 +1848,13 @@ bool bpf_prog_array_compatible(struct bpf_array *array,
 		 */
 		array->aux->owner.type  = fp->type;
 		array->aux->owner.jited = fp->jited;
+		array->aux->owner.expected_attach_type = fp->expected_attach_type;
 		ret = true;
 	} else {
 		ret = array->aux->owner.type  == fp->type &&
-		      array->aux->owner.jited == fp->jited;
+		      array->aux->owner.jited == fp->jited &&
+		      array->aux->owner.expected_attach_type ==
+			fp->expected_attach_type;
 	}
 	spin_unlock(&array->aux->owner.lock);
 	return ret;
